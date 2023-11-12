e131d9e16c25cd75edbee5d76db1e7678353a79d
Candace Saindon <cblsaindon@gmail.com>
2023-11-11 20:24:44 -0700

    slowing down how fast the progress bar loads

--- progressBar.js
+++ progressBar.js
@@ -1,33 +1,63 @@
 /////////////////////PROGRESS BAR///////////////////////
 var powerupReady = false;
+var powerupRate = 20; //HOW FAST THE POWERUP BAR FILLS
+var pageFlag = true;
+var tempDollars = 0;
+var barElem = document.getElementById("myBar");
 
-function moveProgress(direction) {
-    var elem = document.getElementById("myBar");
-    
+
+function moveProgress(direction,bonus) //FILLS IN OR SUBTRACTS FROM THE PROGRESS BAR DEPENDING ON THE POWER-UP STATE
+{
+
     if (direction == "up" && progressWidth >= 100) {
-      //nothing
-      document.getElementById("powerupMsg").innerHTML = "On fire!";
-      powerupReady = true;
-      togglePowerupState(powerupReady);
+
+      pageFlag = "false"; //TRIGGER INCREMENT PAGE
+      document.getElementById("powerupButton").innerHTML = "Turn page!";
+
+      barElem.innerHTML =  "100%";
+      setPowerupButton(true);
 
     } else if (direction == "up") {
-      progressWidth++;
-      elem.style.width = progressWidth + '%';
-      elem.innerHTML = progressWidth * 1  + '%';
+      moveProgressUp(bonus);
+
     } else if (direction == "down" && progressWidth > 0) {
-        //burn it all!
-        //alert("sdafdsfasd");
-        progressWidth--;
-        elem.style.width = progressWidth + '%';
-        elem.innerHTML = progressWidth * 1  + '%';
-    } else {
-        //no action is needed
+      moveProgressDown(bonus);
+    }
+  }
+
+  function moveProgressUp(bonus) {
+    //let elem = document.getElementById("myBar");
+  
+    progressWidth = progressWidth + ((powerupRate/pageCost)*bonus);
+  
+    if (progressWidth > 100) {progressWidth = 100;}
+  
+    if (compactMode) {
+      barText.textContent = Math.round(progressWidth) + "%";
+      barImage.setAttributeNS(null,"width",progressWidth*3);  //WIDTH
+    } else { //FULL MODE
+      barElem.style.width = progressWidth + "%";
+      barElem.innerHTML =  Math.round(progressWidth) + '%';
     }
   }
   
+  function moveProgressDown(bonus) {
+            //burn it all!
+        //progressWidth = progressWidth - (powerupRate/pageCost);
+        progressWidth = progressWidth - (1*bonus);
+        if (progressWidth < 0) {progressWidth = 0;}
+
+        if (compactMode) {
+          barText.textContent = Math.round(progressWidth) + "%";
+          barImage.setAttributeNS(null,"width",progressWidth*3);  //WIDTH
+        } else {
+          barElem.style.width = progressWidth + '%';
+          barElem.innerHTML =  Math.round(progressWidth) + "%";
+        }
+  }
   
-  function clearProgress() {
-    var elem = document.getElementById("myBar");
+  function clearProgress() //BRINGS THE PROGRESSBAR BACK TO 0%
+  {
     var id = setInterval(frame, 10);
     clearInterval(id);  
   }
